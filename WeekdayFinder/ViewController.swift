4a61691397b0c4ab441dc7994bd818bfc3f1213c
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-02-28 00:03:42 -0500

    UI and connection to ViewController

--- WeekdayFinder/ViewController.swift
+++ WeekdayFinder/ViewController.swift
@@ -2,8 +2,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    }}
+    }
+
+}
 
