4c0f648459ab99e360370c554cfdd8654c02c54f
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-02-28 00:24:55 -0500

    findWeekDay() functionality

--- WeekdayFinder/ViewController.swift
+++ WeekdayFinder/ViewController.swift
@@ -8,7 +8,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func findWeekDay(_ sender: UIButton) {
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(dateTextField.text!)
+        dateComponents.month = Int(monthTextField.text!)
+        dateComponents.year = Int(yearTextField.text!)
         
+        let date = calendar.date(from: dateComponents)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        let weekday = dateFormatter.string(from: date!)
+        
+        resultLabel.text = weekday
     }
     
     override func viewDidLoad() {
